286ef11cce107a30eef7200948e051df163a3f9c
Ricky <supericky94@msn.com>
2019-12-12 15:03:13 +0100

    International system and intro of pi.

--- BronchoClass.py
+++ BronchoClass.py
@@ -1,12 +1,13 @@
-from math import sqrt
+from math import sqrt, pi
 
 class EasyBroncho(object):
     """
     Description of the class:
     This class is giving a basic model of the broncho thought as a parallel of a resistive
     element and a compliance.
+    All the physical quantities are expressed in the international system.
     """
-    eta = #1.81 * 10^-5 Pa*s https://www.engineersedge.com/physics/viscosity_of_air_dynamic_and_kinematic_14483.htm
+    eta = 1.81e-5 #Pa*s https://www.engineersedge.com/physics/viscosity_of_air_dynamic_and_kinematic_14483.htm
     def __init__(self, generationNumber=False, length = 0.0, diameter = 0.0, resistance = 0.0, compliance = 0.0):
         if (generationNumber is False):
             self.length = length
@@ -16,10 +17,13 @@ class EasyBroncho(object):
         else:
             self.generationNumber = generationNumber
             self.length = length
-            self.diameter = 1.8*0.5**(self.generationNumber/3.0)
-            self.resistance = (8*eta*self.length)/(pi*(self.diameter/2)**4) #Poiseuille resistance
+            self.diameter = 0.018*0.5**(self.generationNumber/3.0) #1.8 centimeters is 0.018 m
+            self.resistance = (8*EasyBroncho.eta*self.length)/(pi*(self.diameter/2)**4) #Poiseuille resistance --> 
 
 
 if __name__ == "__main__":
-    trachea = EasyBroncho()
-    print ("Trachea diameter is: %d" % trachea.diameter)
+    bifurcation = EasyBroncho(2)
+    print("Bifurcation generation is: %d" % bifurcation.generationNumber)
+    print("Bifurcation diameter is: %f" % bifurcation.diameter)
+    print("Bifurcation length is: %f" % bifurcation.length)
+    print("Bifurcation resistance is: %f" % bifurcation.resistance)
