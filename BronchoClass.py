88e69adfb4dd6d94ad66a0ad727688e08fe60167
Ricky <supericky94@msn.com>
2019-12-16 09:31:25 +0100

    introducing self.compliance = compliance and deleting sqrt

--- BronchoClass.py
+++ BronchoClass.py
@@ -1,4 +1,4 @@
-from math import sqrt, pi
+from math import pi
 import json
 
 class EasyBroncho(object):
@@ -19,12 +19,13 @@ class EasyBroncho(object):
             self.generationNumber = generationNumber
             if (paramsFromJson is False):
                 self.length = length
-                self.diameter = 0.018*0.5**(self.generationNumber/3.0) #1.8 centimeters is 0.018 m
+                self.diameter = 0.018*0.5**(self.generationNumber/3.0) # 1.8 centimeters is 0.018 m
             else:
                 # Reading from json file, this file is static for now. But it would be pointless (at least for now to make it dynamic)
                 self.from_json()
 
-            self.resistance = (8*EasyBroncho.eta*self.length)/(pi*(self.diameter/2)**4) #Poiseuille resistance   
+            self.resistance = (8*EasyBroncho.eta*self.length)/(pi*(self.diameter/2)**4) # Poiseuille resistance   
+            self.compliance = compliance
 
     def from_json(self):
         with open("WeibelsModel_parameters.json") as json_file:
@@ -41,6 +42,6 @@ class EasyBroncho(object):
 if __name__ == "__main__":
     bifurcation = EasyBroncho(1, paramsFromJson=True)
     print("Bifurcation generation is: %d" % bifurcation.generationNumber)
-    print("Bifurcation diameter is: %f m^2" % bifurcation.diameter)
-    print("Bifurcation length is: %f m^2" % bifurcation.length)
+    print("Bifurcation diameter is: %f m" % bifurcation.diameter)
+    print("Bifurcation length is: %f m" % bifurcation.length)
     print("Bifurcation resistance is: %f (Pa*s)/m^3" % bifurcation.resistance)
