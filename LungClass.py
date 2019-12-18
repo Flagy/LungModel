49267bedd91d483477545945ebd8e79515f36b99
Ricky <supericky94@msn.com>
2019-12-18 22:28:39 +0100

    Adding many different features of lung, big change

--- LungClass.py
+++ LungClass.py
@@ -1,33 +1,37 @@
-from math import sqrt
+from math import sqrt, pi
+from BronchoClass import EasyBroncho
+from scipy.integrate import odeint
+import numpy as np
+import matplotlib.pyplot as plt
 
-class EasyLung(object):
+class EasyLung(EasyBroncho):
     """
     Description of the class:
-    This class aims to implement a very basic model of the lung based on Weibel architecture
-    and Lambert model for mechanical properties of the airway.
-    In future a choice of the model might be implemented in the constructor.
+    This class should only expose the methods for solving the differential equations of the model of the EasyBroncho class.
     """
     totGenerations = 27
 
     def __init__(self, numberOfGen):
-        self.numberOfGen = int(numberOfGen) # We may be interested in creating only the first N generations
-        self.tracheaDiameter = 1.8 # cm
-        self.h = 0.5**(1.0/3) # h can be 0.82 in humans if taken as constant or 0.5**(1.0/3) ~ 0.79
-        #self.h = 0.82
-        self.r0 = 0.0
-        self.r1 = 0.0
-        self.r2 = 0.0
-        self.c1 = 0.0
-        self.c2 = 0.0
-        self.createModel()
-
-    def createModel(self):
-        R = [self.r0]
-        C = [self.c0]
-        for gen in range(self.numberOfGen):
-            R.append
+        if numberOfGen < 0 or numberOfGen > EasyLung.totGenerations:
+            raise ValueError
+        super().__init__(generationNumber = numberOfGen, paramsFromJson = True)
     
+    def setModelParams(self, t, Vg, Vc0):
+        self.t = t
+        self.Vg = Vg
+        self.Vc0 = Vc0
+        return True
+    
+    def model(self, Vc, t):
+        self.dVcdt = (1/(self.getTau()))*(self.Vg - Vc)
+        return self.dVcdt
+
+    def solveModel(self):
+        sol = odeint(self.model, self.Vc0, self.t)
+        return sol
+
     def getDiameterFromGen(self, numGen):
+        """Description"""
         return self.d0 * self.h**numGen
 
     def getResistanceFromGen(self, numGen):
@@ -35,5 +39,27 @@ class EasyLung(object):
         d = self.getDiameterFromGen(numGen)
         return (pi*self.P*((d/2.0)**4))/(8.0*eta*self.l)
 
-    def nothing(self, nothing):
-        pass
+
+    def getTau(self):
+        return self.resistance*self.compliance
+
+if __name__ == "__main__":
+    lung = EasyLung(2)
+    lung.compliance = 0.8
+    t0 = 0
+    t_int = 1
+    tf = 100
+    t = np.linspace(t0, t_int, tf)
+    Vg0 = 2
+    f = 1
+    Vg = Vg0*np.sin(2*pi*f*t) - Vg0
+    Vc0 = np.zeros((len(t), ))
+    lung.setModelParams(t, Vg, Vc0)
+    y = lung.solveModel()
+
+    # plot results
+    plt.plot(t,y,'r-',label='Output (y(t))')
+    plt.ylabel('values')
+    plt.xlabel('time')
+    plt.legend(loc='best')
+    plt.show()
